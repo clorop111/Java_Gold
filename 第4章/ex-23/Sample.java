f22e085712912e87eb86a5e73a73d6cbc5eea933
clorop111 <clorop111@icloud.com>
2024-04-16 21:23:35 +0900

    変更3

---
+++ 第4章/ex-23/Sample.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Sample {
+  public static void main(String[] args) {
+    List<Integer> list = List.of(1,2,3,4,5,6,7,8,9,10);
+    List<Integer> result =
+            list.stream()
+                .filter(n -> n % 2 ==0)
+                ./*insert code here */collect(Collectors.toList());
+    result.forEach(System.out::println);                
+  } 
+}
+
+// toList()
+// collect(Collectors.toList())
+// reduce(Collectors.toList())
+// getList()
---
+++ 第4章/ex-24/Department.java
@@ -0,0 +1,21 @@
+public class Department {
+  private String name;
+  public Department(String name) {
+    this.name = name;
+  }
+  public String getName() {
+    return name;
+  }
+  @Override
+  public int hashCode() {
+    return name.hashCode();
+  }
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof Department) {
+      Department other = (Department) obj;
+      return this.name.equals(other.name);
+    }
+    return false;
+  }
+}
