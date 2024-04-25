cd27ae8288f6f9415759e4ec4edbf07d6d014a7d
clorop111 <clorop111@icloud.com>
2024-04-25 23:34:05 +0900

    変更9

---
+++ no1/ex07/Main.java
@@ -0,0 +1,13 @@
+package ex07;
+
+public class Main {
+  public static void main(String[] args) {
+    var sample = new Sample(10) {
+       void modify(int num) {
+        setNum(num);
+      }
+    };
+    sample.modify(100);
+    System.out.println(sample.getNum());
+  }
+}
---
+++ no1/ex07/Sample.java
@@ -0,0 +1,14 @@
+package ex07;
+
+public class Sample {
+  private int num;
+  public Sample(int num) {
+    this.num = num;
+  }
+  public int getNum() {
+    return num;
+  }
+  public void setNum(int num) {
+    this.num = num;
+  }
+}
